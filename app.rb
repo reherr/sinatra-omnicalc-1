67bc4f1354984fcdbedc33abb37e80bd9c7fbeea
Rebecca Herrera <133678924+reherr@users.noreply.github.com>
2024-04-25 18:09:51 +0000

    square root paths and results

--- app.rb
+++ app.rb
@@ -1,10 +1,6 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get("/howdy") do
-  erb(:hello)
-end
-
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
@@ -20,4 +16,14 @@ get("/square/results") do
   @the_num = params.fetch("users_number").to_f
   @the_result = @the_num ** 2
   erb(:square_results)
+end
+
+get("/square_root/new") do 
+  erb(:square_root_new)
+end
+
+get("/square_root/results") do
+@the_num = params.fetch("user_number").to_f
+@the_result = Math.sqrt(@the_num)
+  erb(:square_root_results)
 end
